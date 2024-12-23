53e8deaa492bc9df1e9b06442817c5f923a2fe74
raythonic <raythonic@gmail.com>
2024-12-23 13:50:05 -0800

    Updated publish script to support versioning in mongodb

--- publish.sh
+++ publish.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+build_version=$(cat build-version.txt)
+
+# Split the version string into an array
+IFS='.' read -r -a version_parts <<< "$build_version"
+
+# Add 1 to the last part (patch version)
+((version_parts[2]++))
+
+# Join the version parts back together
+new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+echo "$new_version" > build-version.txt
+
+# Update this app's version number in the mongodb database
+/home/rwalk/bin/update-myconfig.sh "MyNotes" "version" "$new_version"
+
 cp src/mynotes.py $HOME/services/.
 cp src/mynotes.sh $HOME/services/mynotes
 cp src/google-calendar.py $HOME/services/.
