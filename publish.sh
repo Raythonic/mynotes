68651769841c5dfc0ee8a328905e7e4eef794819
raythonic <raythonic@gmail.com>
2024-12-23 14:32:22 -0800

    Updates to myconfig script

--- publish.sh
+++ publish.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-build_version=$(cat build-version.txt)
-
-# Split the version string into an array
-IFS='.' read -r -a version_parts <<< "$build_version"
-
-# Add 1 to the last part (patch version)
-((version_parts[2]++))
-
-# Join the version parts back together
-new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
-
-echo "$new_version" > build-version.txt
-
 # Update this app's version number in the mongodb database
-/home/rwalk/bin/update-myconfig.sh "MyNotes" "version" "$new_version"
+/home/rwalk/bin/update-myconfig.sh "MyNotes" "version" "BUILD-VERSION"
 
 cp src/mynotes.py $HOME/services/.
 cp src/mynotes.sh $HOME/services/mynotes
