09aebf4451fdb5fe8ce1ad1a51b2ca27dcae2834
raythonic <raythonic@gmail.com>
2025-02-25 21:21:42 -0800

    Testing new config directives

--- publish.sh
+++ publish.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-source /home/rwalk/bin/bash_ext > /dev/null
+. /home/rwalk/bin/publisher
 
-cp src/mynotes.py $HOME/services/.
-cp src/mynotes.sh $HOME/services/mynotes
-cp src/google-calendar.py $HOME/services/.
 cp google-api/*.json $HOME/sensitive/.
-
-chmod +x $HOME/services/mynotes.py
-chmod +x $HOME/services/google-calendar.py 
-chmod +x $HOME/services/mynotes
-
-# Place command in bin/
-rm $HOME/bin/mynotes
-ln -s $HOME/services/mynotes $HOME/bin/mynotes 
 
 publish
